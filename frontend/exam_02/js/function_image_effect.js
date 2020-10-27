2dd0ec7dc7f04227445f3ffde18cc4e65cdd787d
ministori <ministori@gmail.com>
2020-10-27 17:57:40 +0900

    Add files via upload

--- frontend/exam_02/js/function_image_effect.js
+++ frontend/exam_02/js/function_image_effect.js
@@ -37,6 +37,9 @@ function rightFade(){
 
 }
 
+function scale(currentScroll, inputMin, inputMax, outputMin, outputMax){
+    return (currentScroll - inputMin) * (outputMax - outputMin) / (inputMax - inputMin) + outputMin;
+}
 
 // 이벤트, 기능 실행
 $(function(){
@@ -46,5 +49,48 @@ $(function(){
     nextIndex = 1;
 
     visualPlay();
+
+    var last = 0;
+    $(document).on('scroll', function(){
+
+        //console.log( $(this).scrollTop() );
+
+        var st = $(this).scrollTop();
+        if( st > last ){
+            // down
+            $('.header').addClass('scroll');
+        } else {
+            // up
+            $('.header').removeClass('scroll');
+        }
+
+        if( st > 10 ){
+            $('.header').addClass('bg-white');
+        } else {
+            $('.header').removeClass('bg-white');
+        }
+
+        last = st;
+
+        // 3300 ~ 4400
+        if( $(this).scrollTop() >= 3300 && $(this).scrollTop() <= 4400 ){
+
+            var scrollValue = scale($(this).scrollTop(), 3300, 4400, 0, 50);
+            //console.log( scale($(this).scrollTop(), 3300, 4400, 0, 50) );
+            $('.tablet-image').css({
+                transform:'translateX(' + (-100 + scrollValue) + '%)'
+            });
+            
+        }
+
+    });
+
+    $('.gallery-image-item').on('mouseenter', function(){
+        $(this).addClass('scale');
+    });
+
+    $('.gallery-image-item').on('mouseleave', function(){
+        $(this).removeClass('scale');
+    });
 
 });
